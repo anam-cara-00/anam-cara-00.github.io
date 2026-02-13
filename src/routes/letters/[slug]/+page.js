c63c27e7bcab064f2348bf37e975ed839584ff9b
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-02-13 19:41:25 +0000

    style(prettier): adjust code style

--- src/routes/letters/[slug]/+page.js
+++ src/routes/letters/[slug]/+page.js
@@ -3,22 +3,22 @@ import letters from '$lib/letters.json';
 
 /** @type {import('./$types').PageLoad} */
 export function load({ params }) {
-	const slug = params.slug;
+  const slug = params.slug;
 
-	// check if slug exists
-	if (!letters[slug]) {
-		throw error(404, 'Carta no encontrada');
-	}
+  // check if slug exists
+  if (!letters[slug]) {
+    throw error(404, 'Carta no encontrada');
+  }
 
-	const { title, emoji, date, data } = letters[slug];
+  const { title, emoji, date, data } = letters[slug];
 
-	const bytes = Uint8Array.from(atob(data), (c) => c.charCodeAt(0));
-	const content = new TextDecoder().decode(bytes);
+  const bytes = Uint8Array.from(atob(data), (c) => c.charCodeAt(0));
+  const content = new TextDecoder().decode(bytes);
 
-	return {
-		title,
-		content,
-		emoji,
-		date
-	};
+  return {
+    title,
+    content,
+    emoji,
+    date
+  };
 }
